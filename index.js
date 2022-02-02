3899b1d328464003f8df8b19fa88803a46e29474
SenhorDK <48421164+SenhorDK@users.noreply.github.com>
2022-02-01 21:00:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const tmi = require('tmi.js');
 
-const client = new tmi.Client({
-	options: { debug: true },
-	identity: {
-		username: 'senhordk13',
-		password: 'oauth:rsg35ud0kjsmcerhj1oq2krkn7u82u'
-	},
-	channels: [ 'blxckoutz' ]
+const opts = {
+  connection: { reconnect: true },
+  identity: {
+    username: 'senhordk13',
+    password: 'oauth:rsg35ud0kjsmcerhj1oq2krkn7u82u'
+  },
+  channels: ['blxckoutz'],
+};
+
+const client = new tmi.client(opts);
+
+client.on('connected', (ip, port) => {
+  console.log(`O bot entrou no endereço: ${ip}:${port}`);
 });
+
+const channels = {};
 
 client.on('message', (channel, state, message, self) => {
   if (self) return;
